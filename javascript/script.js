56de7e3eea7953c2d0942ea6cf23adf644a2112e
Diego Lucas <diegolucas@outlook.ie>
2023-01-30 12:33:24 +0000

    1. Upload files previously created

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+// Typewriter
+const instance = new Typewriter('#typewriter', {
+    loop: true,
+    delay: 50,
+  });
+
+  instance.typeString('Escritor')
+    .pauseFor(1000)
+    .deleteAll(15)
+    .typeString('Autor de <em>As Aventuras de Alex Canyon</em>')
+    .pauseFor(1000)
+    .deleteAll(15)
+    .start();
+
+// 
