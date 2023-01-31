b78020de3bc4d8390f18372ae7d6d0f115f4c50e
Diego Lucas <diegolucas@outlook.ie>
2023-01-31 14:06:40 +0000

    2. Created pages and added animation

--- javascript/script.js
+++ javascript/script.js
@@ -6,10 +6,10 @@ const instance = new Typewriter('#typewriter', {
 
   instance.typeString('Escritor')
     .pauseFor(1000)
-    .deleteAll(15)
+    .deleteAll(10)
     .typeString('Autor de <em>As Aventuras de Alex Canyon</em>')
     .pauseFor(1000)
-    .deleteAll(15)
+    .deleteAll(10)
     .start();
 
 // 
