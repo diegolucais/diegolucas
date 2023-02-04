e6fd8c16480b45af057606917ab5104c28e4f24c
Diego Lucas <diegolucas@outlook.ie>
2023-02-04 19:51:18 +0000

    9. Added favicon and formatted code

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,13 @@
 // Typewriter
 const instance = new Typewriter('#typewriter', {
-    loop: true,
-    delay: 50,
-  });
+  loop: true,
+  delay: 50,
+});
 
-  instance.typeString('Escritor')
-    .pauseFor(1000)
-    .deleteAll(10)
-    .typeString('Autor de <em>As Aventuras de Alex Canyon</em>')
-    .pauseFor(1000)
-    .deleteAll(10)
-    .start();
-
-// 
+instance.typeString('Escritor')
+  .pauseFor(1000)
+  .deleteAll(10)
+  .typeString('Autor de <em>As Aventuras de Alex Canyon</em>')
+  .pauseFor(1000)
+  .deleteAll(10)
+  .start();
