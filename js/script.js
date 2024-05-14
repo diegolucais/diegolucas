441255246a2799dd3e30149704ff40de783f8e35
Diego Lucas <diegolucas@outlook.ie>
2024-05-14 23:27:36 +0100

    Third commit
    javascript – sticky navbar & active links.

--- js/script.js
+++ js/script.js
@@ -2,37 +2,51 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      //active navbar links
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+      });
+    }
+  });
+
+// sticky header
+let header = document.querySelector('header');
+
+header.classList.toggle('sticky', window.scrollY > 100);
+
+// remove toggle icon and navbar when click navbar links (scroll)
 
 
-    // sticky header
-
-
-    // remove toggle icon and navbar when click navbar links (scroll)
-
-
-    // animation footer on scroll
+// animation footer on scroll
 
 }
 
-// Typewriter
+// typewriter
 const instance = new Typewriter('.text-animate', {
-    loop: true,
-    delay: 60,
-  });
-  
-  instance.typeString('Escritor')
-    .pauseFor(1000)
-    .deleteAll(10)
-    .typeString('Autor de As Aventuras de Alex Canyon')
-    .pauseFor(1000)
-    .deleteAll(10)
-    .typeString('Os Escaravelhos de Seth - Outubro de 2024')
-    .pauseFor(1000)
-    .deleteAll(10)
-    .start();
+  loop: true,
+  delay: 60,
+});
+
+instance.typeString('Escritor')
+  .pauseFor(1000)
+  .deleteAll(10)
+  .typeString('Autor de As Aventuras de Alex Canyon')
+  .pauseFor(1000)
+  .deleteAll(10)
+  .typeString('Os Escaravelhos de Seth - Outubro de 2024')
+  .pauseFor(1000)
+  .deleteAll(10)
+  .start();
