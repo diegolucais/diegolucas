5164f8591f57e6039aff7c2af201a3a141e7a12c
Diego Lucas <diegolucas@outlook.ie>
2024-05-16 23:37:30 +0100

    Fourth commit
    Added Media Query to style.css.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -28,7 +33,8 @@ let header = document.querySelector('header');
 header.classList.toggle('sticky', window.scrollY > 100);
 
 // remove toggle icon and navbar when click navbar links (scroll)
-
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 
 // animation footer on scroll
 
